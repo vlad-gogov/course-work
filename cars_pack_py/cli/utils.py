888473163ceafb624f008a2fe1ea2437eba4a38e
vla5924 <vla5924@gmail.com>
2021-11-26 14:19:27 +0300

    paralellized main loop
    vladik dobei

--- cars_pack_py/cli/utils.py
+++ cars_pack_py/cli/utils.py
@@ -1,5 +1,7 @@
 import csv
 from os import write
+from concurrent.futures import ThreadPoolExecutor
+
 from ..backend.service_device import ServiceDevice
 
 
@@ -17,76 +19,63 @@ def expected_value(r: list, g: list) -> list:
     return result
 
 
-def Test():
+def thread_function(thread_id: int):
+    asfile = open(
+        f"cars_pack_py\\results\\test_{thread_id}.csv", "w", encoding='utf-8')
+    fieldnames = ['#', 'count cars', 'lamb1', 'lamb2', 'r1', 'r2', 'g1', 'g2',
+                  'gamma1', 'gamma2', "average length pack 1:", "average length pack 2:"]
+    writer = csv.DictWriter(asfile, fieldnames=fieldnames)
+    writer.writeheader()
 
-    with open("test.csv", "w", encoding='utf-8') as asfile:
-        fieldnames = ['№', 'count cars', 'lamb1', 'r1', 'g1',
-                      'gamma1', "average length pack 1:", 'lamb2', 'r2', 'g2', 'gamma2', "average length pack 2:"]
-        writer = csv.DictWriter(asfile, fieldnames=fieldnames)
-        writer.writeheader()
+    lamb = [0.1 * (thread_id + 1), 0.1]
+    time = 100
+    r = [0.1, 0.1]
+    g = [0.1, 0.1]
+    time_service = [[60, 0.5], [3], [60, 0.5], [0, 0.5], [3]]
 
-        lamb = [0.1, 0.1]
-        time = 100
-        r = [0.1, 0.1]
-        g = [0.1, 0.1]
-        time_service = [[60, 0.5], [3], [60, 0.5], [0, 0.5], [3]]
-
-        f = True
-        count_cars = 5000
-        sd = ServiceDevice()
-        l = 0
-
+    f = True
+    count_cars = 5000
+    sd = ServiceDevice()
+    l = thread_id * (9 ** 5) + 1
+    for _ in range(9):
         for _ in range(9):
-
             for _ in range(9):
-
                 for _ in range(9):
-
                     for _ in range(9):
-
-                        for _ in range(9):
-
-                            for _ in range(9):
-                                prev = sd.Start(
-                                    lamb, time, r, g, time_service, count_cars)
-                                b = count_cars
-
-                                while f:
-                                    b += count_cars
-                                    a = sd.Start(lamb, time, r, g,
-                                                 time_service, b)
-                                    if abs(a[0] - prev[0]) <= 1 and abs(a[1] - prev[1]) <= 1:
-                                        avglengt = expected_value(r, g)
-                                        writer.writerow({'№': l, 'count cars': b, 'lamb1': lamb[0], 'r1': r[0], 'g1': g[0],
-                                                         'gamma1': a[0], "average length pack 1:": avglengt[0], 'lamb2': lamb[1], 'r2': r[1], 'g2': g[1], 'gamma2': a[1], "average length pack 2:": avglengt[1]})
-                                        f = False
-                                    prev = a
-                                f = True
-                                l += 1
-                                print(l)
-                                g[1] += 0.1
-
-                            g[1] = 0.1
-                            g[0] += 0.1
-
-                        g[1] = 0.1
-                        g[0] = 0.1
-                        r[1] += 0.1
-
+                        prev = sd.Start(
+                            lamb, time, r, g, time_service, count_cars)
+                        b = count_cars
+                        while f:
+                            b += count_cars
+                            a = sd.Start(lamb, time, r, g,
+                                         time_service, b)
+                            if abs(a[0] - prev[0]) <= 1 and abs(a[1] - prev[1]) <= 1:
+                                avglengt = expected_value(r, g)
+                                writer.writerow({'#': l, 'count cars': b, 'lamb1': lamb[0], 'r1': r[0], 'g1': g[0],
+                                                 'gamma1': a[0], "average length pack 1:": avglengt[0], 'lamb2': lamb[1], 'r2': r[1], 'g2': g[1], 'gamma2': a[1], "average length pack 2:": avglengt[1]})
+                                f = False
+                            prev = a
+                        f = True
+                        print(l)
+                        l += 1
+                        g[1] += 0.1
                     g[1] = 0.1
-                    g[0] = 0.1
-                    r[1] = 0.1
-                    r[0] += 0.1
-
+                    g[0] += 0.1
                 g[1] = 0.1
                 g[0] = 0.1
-                r[1] = 0.1
-                r[0] = 0.1
-                lamb[1] += 0.1
-
+                r[1] += 0.1
             g[1] = 0.1
             g[0] = 0.1
             r[1] = 0.1
-            r[0] = 0.1
-            lamb[1] += 0.1
-            lamb[0] += 0.1
+            r[0] += 0.1
+        g[1] = 0.1
+        g[0] = 0.1
+        r[1] = 0.1
+        r[0] = 0.1
+        lamb[1] += 0.1
+    asfile.close()
+
+
+def Test():
+    with ThreadPoolExecutor(max_workers=12) as executor:
+        executor.map(thread_function, range(9))
